e58d6d8ad0b98aa64e3e668ac9c18c3f4039dd58
buzzwalter <buzzwalter2533@gmail.com>
2024-03-22 14:00:42 +0100

    implemented button and tracing for gui

--- gui.py
+++ gui.py
@@ -1,17 +1,18 @@
 from matplotlib.widgets import RectangleSelector
 from matplotlib.widgets import Button
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
-from move import set_scan_properties
-from move import reference_motor
-from move import snake_scan
+from move import *
 import smaract.ctl as ctl
 
 a_max = []
 b_max = []
 a_min = []
 b_min = []
+
+is_running = False  # Global flag to control the loop
 
 # callback for handling events when rectangular area is selected
 def line_select_callback(eclick, erelease, a_max=a_max, b_max=b_max, a_min=a_min, b_min=b_min):
@@ -40,45 +41,116 @@ def toggle_selector(event):
         print(' RectangleSelector activated.')
         toggle_selector.RS.set_active(True)
 
+# stop button for gui based interruption 
+def stop_scanning(event):
+    # event to handle button press from the GUI
+    global is_running
+    is_running = not is_running
+    print("Stopping scanning...")
+
+
 # plots available space for the motor to move
-def get_plot(N=1000, x_zero=14,y_zero=13):
+def get_plot():#(N=1000, x_zero=14,y_zero=13):
     # N number of points to use
     # x_zero referenced zero for the laser alignment along axis a
     # y_zero referenced zero for the laser alignment along axis b
 
-    fig, current_ax = plt.subplots()                 # make a new plotting range
+    fig, current_ax = plt.subplots(figsize=(15, 12))                 # make a new plotting range
     plt.subplots_adjust(bottom=0.2) # Adjust the bottom to make room for the button
-    N = 1000                                       # If N is large one can see the axes
-    plate_extent = 24
-    x = -np.linspace(0.0, plate_extent, N)+x_zero               # improvement by use blitting!
-    y = -x-y_zero+x_zero
 
-    plt.scatter(0,0,marker='+',c='r') # plot where the zero is for the plate
-    # plot zero for laser in blue
-    plt.scatter(x, (plate_extent/2-y_zero)*np.ones(len(x)),c='b', marker='.', alpha=.02)  
-    plt.scatter((-plate_extent/2+x_zero)*np.ones(len(x)), y,c='b', marker='.', alpha=.02)  
-    plt.legend(['plate','laser'])
-    plt.axis([max(x), min(x), min(y), max(y)])
-    plt.xlabel('axis a')
-    plt.ylabel('axis b')
-    plt.title('Motor Scan Area')
-    print("\n      click  -->  release")
 
-    # would like to implement this section with the stop_scanning button rather than use ctl+D but would need to multi-thread
-    #stop_button_ax = plt.axes([0.7, 0.05, 0.1, 0.075])  # [left, bottom, width, height]
-    #stop_button = Button(stop_button_ax, 'Stop', color='red', hovercolor='salmon')
-    #stop_button.on_clicked(stop_scanning)
+    stop_button_ax = plt.axes([0.7, 0.05, 1, 0.09])  # [left, bottom, width, height]
+    stop_button = Button(stop_button_ax, 'Toggle Scanning', color='y', hovercolor='salmon')
+    stop_button.on_clicked(stop_scanning)
 
     # drawtype is 'box' or 'line' or 'none'
     toggle_selector.RS = RectangleSelector(current_ax, line_select_callback,drawtype='box', useblit=True,button=[1, 3], minspanx=5, minspany=5,spancoords='pixels',interactive=True)
     plt.connect('key_press_event', toggle_selector)
-    plt.show(block=True)
 
-# main code block
-try:
+    xdata, ydata = [0], [np.sin(0)]
+    return plt.plot([], [], 'teal')
+
+
+
+# Update function for FuncAnimation
+def update(frame):
+    global is_running
+    global d_handle
+    global loop_index
+    num_channels = 2
+    step_size=int(5e7)
+
+    if is_running :
+
+        # For demonstration, we'll just simulate adding a new point to the line
+        xdata, ydata = line.get_data()
+        xdata = list(xdata)
+        ydata = list(ydata)
+        line.set_data(xdata, ydata)
+
+        boundaries_container = [[b_min[0],b_max[0]],[a_min[0],a_max[0]]]
+    
+        try:
+            if num_channels != 2: # would like to use get properties to retrieve number of channels property
+                raise ChannelError("Number of channels must be 2.")
+            channel_b = 0
+            channel_a = 1
+            b_boundaries, a_boundaries = boundaries_container
+            N_points_b = int(np.ceil((b_boundaries[1] - b_boundaries[0])*1e9 / step_size)) 
+
+            if loop_index <(N_points_b // 2): # two movements in the b axis per loop so we int divide by 2
+                print('loop number {} out of {}'.format(loop_index,N_points_b//2) )
+                for boundary in a_boundaries:
+                    ctl.Move(d_handle,channel_a,int(boundary*1e9)) # convert mouse click to pm
+                    xdata.append( boundary)
+                    wait_for_movement_settled(d_handle, channel_a)
+                    ctl.Move(d_handle,channel_b,int(b_boundaries[0]*1e9)+step_size*loop_index)
+                    wait_for_movement_settled(d_handle, channel_b)
+                    ydata.append(-(int(b_boundaries[0])+step_size*loop_index*1e-9))
+                loop_index=loop_index+1
+            if (N_points_b%2) and (loop_index==N_points_b // 2):
+                ctl.Move(d_handle,channel_b,int(b_boundaries[0])+step_size*((N_points_b // 2)+1)) # ensure movement catches the odd parity mismatch from the for-loop
+                xdata.append(boundary)
+                ydata.append(-(int(b_boundaries[0]*1e9)+step_size*((N_points_b // 2)+1)*1e-9))
+        except ctl.Error as e:
+            # Passing an error code to "GetResultInfo" returns a human readable string
+            # specifying the error.
+            print("MCS2 {}: {}, error: {} (0x{:04X}) in line: {}.".format(e.func, ctl.GetResultInfo(e.code), ctl.ErrorCode(e.code).name, e.code, (sys.exc_info()[-1].tb_lineno)))
+        except ChannelError as ce:
+            print(f"Channel Error: {ce}")
+        return line,
+    else:
+        return line,
+
+
+
+def init():
+    x_zero=12  
+    y_zero=12
+    N = 1000                                       # If N is large one can see the axes
+    plate_extent = 24
+    x = -np.linspace(0.0, plate_extent, N)+x_zero              
+    y = -x-y_zero+x_zero
+    ax.set_xlim(xmax=max(x), xmin=min(x))
+    ax.set_ylim(ymin=min(y), ymax=max(y))
+    ax.set_xlabel('axis a')
+    ax.set_ylabel('axis b')
+    ax.set_title('Motor Scan Area')
+    ax.scatter(0,0,marker='+',c='r') # plot where the zero is for the plate
+    # plot zero for laser in blue
+    ax.scatter(x, (plate_extent/2-y_zero)*np.ones(len(x)),c='b', marker='.',s=.1)  
+    ax.scatter((-plate_extent/2+x_zero)*np.ones(len(x)), y,c='b', marker='.',s=.1) 
+    line.set_data(xdata, ydata)
+    return line,
+
+
+# main execution blocks start here
+
+# establish a connection with the controller
+try:        
     buffer = ctl.FindDevices("")
     if len(buffer) == 0:       
-        sys.exit(1)
+         sys.exit(1)
     locators = buffer.split("\n")
     for locator in locators:
         print("MCS2 available devices: {}".format(locator))
@@ -88,13 +160,38 @@ except:
     sys.exit(1)
 
 try:
-    get_plot()
+    # Initialize plot
+    fig, ax = plt.subplots(figsize=(15, 12))
+    xdata, ydata = [0], [np.sin(0)] # potentially rather start with 
+    line, = plt.plot([], [], 'r',linestyle='-')
+    # add widgets
+    plt.subplots_adjust(bottom=0.2) # Adjust the bottom to make room for the button
+    stop_button_ax = plt.axes([0.7, 0.05, 0.1, 0.075])  # [left, bottom, width, height]
+    stop_button = Button(stop_button_ax, 'Toggle Scanning', color='red', hovercolor='salmon')
+    stop_button.on_clicked(stop_scanning)
+    toggle_selector.RS = RectangleSelector(ax, line_select_callback,drawtype='box', useblit=True,button=[1, 3], minspanx=5, minspany=5,spancoords='pixels',interactive=True)
+    plt.connect('key_press_event', toggle_selector)
+
     d_handle = ctl.Open(locators[0])
     print("MCS2 opened {}.".format(locators[0]))
     reference_motor(d_handle)
     set_scan_properties(d_handle)
-    boundaries_container = [[b_min[0],b_max[0]],[a_min[0],a_max[0]]]
-    snake_scan(d_handle,boundaries_container)
+    loop_index = 0
+
+    ani = FuncAnimation(fig, update, frames=np.linspace(0, 2 * np.pi, 128),
+                    init_func=init, blit=True, repeat=True)
+
+    plt.show()
+    ctl.Close(d_handle)
+    print("MCS2 close.")
+    print("*******************************************************")
+
+    
+ 
 except Exception as e:
     print("Main process failed")
     raise
+
+
+
+
--- move.py
+++ move.py
@@ -1,8 +1,9 @@
 import sys
 import smaract.ctl as ctl
 import numpy as np
+import time
 
-should_continue = True  # Global flag to control the loop
+
 
 class ChannelError(Exception):
     """Custom exception for channel errors."""
@@ -38,54 +39,6 @@ def reference_motor(d_handle, move_velocity =  int(2e9),num_channels=2):
             # specifying the error
             print("MCS2 {}: {}, error: {} (0x{:04X}) in line: {}.".format(e.func, ctl.GetResultInfo(e.code), ctl.ErrorCode(e.code).name, e.code, (sys.exc_info()[-1].tb_lineno)))
 
-# stop button for gui based interruption 
-def stop_scanning(event):
-    # event to handle button press from the GUI
-    global should_continue
-    should_continue = False
-    print("Stopping scanning...")
-
-# generate snake movement for motor in the selected area
-def snake_scan(d_handle, boundaries_container, step_size=int(5e7),num_channels=2,restart_scan=True):
-    # d_handle of motor for interacting in the Smaract API
-    # boundaries_container with as many entries as channels, starting with channel 0 and containing the min max pairs (min_pos_i,max_pos_i)
-    # step_size to move motor, set to 50 micron
-    # num_channels set to 2 since the snake movement only makes sense for 2 channels
-    # restart_scan to toggle the movement on or off
-    try:
-        if num_channels != 2: # would like to use get properties to retrieve number of channels property
-            raise ChannelError("Number of channels must be 2.")
-        channel_b = 0
-        channel_a = 1
-        b_boundaries, a_boundaries = boundaries_container
-        N_points_b = int(np.ceil((b_boundaries[1] - b_boundaries[0])*1e9 / step_size)) 
-        while restart_scan:
-            for i in range(N_points_b // 2): # two movements in the b axis per loop so we int divide by 2
-                print('loop number {} out of {}'.format(i,N_points_b//2) )
-                for boundary in a_boundaries:
-                    ctl.Move(d_handle,channel_a,int(boundary*1e9)) # convert mouse click to pm
-                    wait_for_movement_settled(d_handle, channel_a)
-                    ctl.Move(d_handle,channel_b,step_size*i)
-                    wait_for_movement_settled(d_handle, channel_b)
-            if N_points_b%2:
-                ctl.Move(d_handle,channel_b,step_size*((N_points_b // 2)+1)) # ensure movement catches the odd parity mismatch from the for-loop
-
-        ctl.Close()
-    except ctl.Error as e:
-        # Passing an error code to "GetResultInfo" returns a human readable string
-        # specifying the error.
-        print("MCS2 {}: {}, error: {} (0x{:04X}) in line: {}.".format(e.func, ctl.GetResultInfo(e.code), ctl.ErrorCode(e.code).name, e.code, (sys.exc_info()[-1].tb_lineno)))
-    except ChannelError as ce:
-        print(f"Channel Error: {ce}")
-    finally:
-        # Before closing the program the connection to the device must be closed by calling "Close". Also we want to set channel 1 back to its original value
-        if d_handle != None:
-            print("channel 0/1 final position: ", ctl.GetProperty_i64(d_handle,0,ctl.Property.POSITION),ctl.GetProperty_i64(d_handle,0,ctl.Property.POSITION))
-            ctl.Close(d_handle)
-            print("MCS2 close.")
-            print("*******************************************************")
-            print("Done. Press return to exit.")
-            input()
 
 # wait for movement to settle before attempting to move the motor again
 def wait_for_movement_settled(d_handle, channel):
@@ -106,4 +59,5 @@ def wait_for_movement_settled(d_handle, channel):
         # specifying the error.
         print("MCS2 {}: {}, error: {} (0x{:04X}) in line: {}.".format(e.func, ctl.GetResultInfo(e.code), ctl.ErrorCode(e.code).name, e.code, (sys.exc_info()[-1].tb_lineno)))
 
- 
+
+
